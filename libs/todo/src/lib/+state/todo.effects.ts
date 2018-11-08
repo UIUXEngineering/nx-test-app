bd750d3ec64beaf83906bf6fca22fa1f0c1e0ea4
jorta <jorta@octolabs.io>
2018-11-08 07:06:05 -0600

    refactor(ngrx feature): remove DataPersisance

--- libs/todo/src/lib/+state/todo.effects.ts
+++ libs/todo/src/lib/+state/todo.effects.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Effect, Actions } from '@ngrx/effects';
+import { Effect, Actions, ofType } from '@ngrx/effects';
 import { DataPersistence } from '@nrwl/nx';
-
+import { catchError, map } from 'rxjs/operators';
+import { of } from 'rxjs';
 import { TodoPartialState } from './todo.reducer';
 import {
   LoadTodo,
@@ -13,17 +14,28 @@ import {
 @Injectable()
 export class TodoEffects {
   @Effect()
-  loadTodo$ = this.dataPersistence.fetch(TodoActionTypes.LoadTodo, {
-    run: (action: LoadTodo, state: TodoPartialState) => {
-      // Your custom REST 'load' logic goes here. For now just return an empty list...
+  loadTodo$ = this.actions$.pipe(
+    ofType(TodoActionTypes.LoadTodo),
+    map((action: TodoActionTypes.LoadTodo) => {
       return new TodoLoaded([]);
-    },
+    }),
+    catchError((error, caught) => {
+      return of(new TodoLoadError(caught));
+    })
+  );
 
-    onError: (action: LoadTodo, error) => {
-      console.error('Error', error);
-      return new TodoLoadError(error);
-    }
-  });
+  // @Effect()
+  // loadTodo$ = this.dataPersistence.fetch(TodoActionTypes.LoadTodo, {
+  //   run: (action: LoadTodo, state: TodoPartialState) => {
+  //     // Your custom REST 'load' logic goes here. For now just return an empty list...
+  //     return new TodoLoaded([]);
+  //   },
+  //
+  //   onError: (action: LoadTodo, error) => {
+  //     console.error('Error', error);
+  //     return new TodoLoadError(error);
+  //   }
+  // });
 
   constructor(
     private actions$: Actions,
