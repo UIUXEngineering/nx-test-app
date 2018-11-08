69895ae419fd9d39b58da7fe357e272fa6adfcec
jorta <jorta@octolabs.io>
2018-11-08 05:01:44 -0600

    feat(ngrx): test in component

--- apps/my-app/src/app/app.component.spec.ts
+++ apps/my-app/src/app/app.component.spec.ts
@@ -1,8 +1,15 @@
 import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Store, StoreModule } from '@ngrx/store';
-import { Entity, initialState, TodoFacade, todoReducer, TodoState } from '@ngrx7/todo';
-import { TodoLoaded } from '../../../../libs/todo/src/lib/+state/todo.actions';
+import {
+  Entity,
+  initialState,
+  TodoFacade,
+  todoReducer,
+  TodoState,
+  TODO_FEATURE_KEY,
+  TodoLoaded
+} from '@ngrx7/todo';
 import { AppComponent } from './app.component';
 
 interface TestSchema {
@@ -15,8 +22,8 @@ describe('AppComponent', () => {
   let store: Store<TestSchema>;
   let createTodo;
 
-  beforeEach((done) => {
-    createTodo = (id: string, name = ''): Entity => ({
+  beforeEach(( done ) => {
+    createTodo = ( id: string, name = '' ): Entity => ({
       id,
       name: name || `name-${id}`
     });
@@ -25,11 +32,11 @@ describe('AppComponent', () => {
       imports: [
         RouterTestingModule,
         StoreModule.forRoot({}),
-        StoreModule.forFeature('todo', todoReducer, { initialState }),
+        StoreModule.forFeature(TODO_FEATURE_KEY, todoReducer, { initialState })
         // EffectsModule.forFeature([TodoEffects])
       ],
-      providers: [TodoFacade],
-      declarations: [AppComponent]
+      providers: [ TodoFacade ],
+      declarations: [ AppComponent ]
     }).compileComponents().then(() => {
       facade = TestBed.get(TodoFacade);
       store = TestBed.get(Store);
@@ -64,7 +71,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const comp = fixture.componentInstance;
     comp.ngOnInit();
-    store.dispatch(new TodoLoaded([createTodo('AAA'), createTodo('BBB')]));
+    store.dispatch(new TodoLoaded([ createTodo('AAA'), createTodo('BBB') ]));
     console.log(comp.todoResult);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
@@ -72,6 +79,9 @@ describe('AppComponent', () => {
       'Welcome to my-app!'
     );
 
-    expect(comp.todoResult).toEqual([{id: 'AAA', name: 'name-AAA'}, {id: 'BBB', name: 'name-BBB'}]);
+    expect(comp.todoResult).toEqual([ { id: 'AAA', name: 'name-AAA' }, {
+      id: 'BBB',
+      name: 'name-BBB'
+    } ]);
   });
 });
--- libs/todo/src/index.ts
+++ libs/todo/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/+state/todo.actions';
 export * from './lib/+state/todo.facade';
 export * from './lib/+state/todo.reducer';
 export * from './lib/+state/todo.selectors';
